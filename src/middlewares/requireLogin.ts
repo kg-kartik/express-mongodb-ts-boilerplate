0a7258eab25c56ff9dd7591347b3149400af6035
Kartik Goel <goel.kartik39@gmail.com>
2022-11-09 10:42:58 +0530

    Added error handling for invalid jwt signature

--- src/middlewares/requireLogin.ts
+++ src/middlewares/requireLogin.ts
@@ -1,18 +1,20 @@
 import jwt from "jsonwebtoken";
-import { Response, NextFunction } from "express";
+import {Response,NextFunction} from "express"
 import RequestWithUser from "../Types/RequestWithUser";
 import UsersModel from "../models/Users";
 import Users from "../Types/Users";
+import ErrorResponse from "../utils/ErrorResponse";
 
 require("dotenv").config();
 
 const secret = process.env.JWT_SECRET;
-interface JwtPayload {
-    email: String;
+interface JwtPayload{
+    email:String
 }
 
-export const requireLogin = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+export const requireLogin =  async (req:RequestWithUser, res:Response, next:NextFunction) => {
     const { authorization } = req.headers;
+
     if (!authorization) {
         return res.status(401).json({
             error: "no headers provided"
@@ -21,15 +23,23 @@ export const requireLogin = async (req: RequestWithUser, res: Response, next: Ne
     //Get the token from Bearer "token"
     const token = authorization.replace("Bearer ", "");
 
-    const { email } = jwt.verify(token, secret) as JwtPayload;
+    console.log(token,"token");
 
-    try {
+    const payload= await jwt.verify(token, secret) as JwtPayload;
+
+    console.log(payload,"email");
+
+    if(payload){
+
+        const {email} = payload;
+
         req.user = (await UsersModel.findOne({
             email
         })) as Users;
-    } catch (e) {
-        console.log(e, "err");
+        next();
     }
-
-    next();
+    else{
+        return next(new ErrorResponse("You must be logged in",401));
+    }
+    
 };
